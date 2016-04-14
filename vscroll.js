514770023bb3b3a6a944b055bd57c08d19962259
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2016-04-14 18:06:08 +0300

    Data window algorithm;

--- vscroll.js
+++ vscroll.js
@@ -5,16 +5,54 @@
 
 angular.module('vscroll', [])
     .service('vscrollService', function () {
-        return function (context) {
-            context = angular.extend({
-                cursor: 0,
-                page: 0
-            }, context);
+        return function (settings) {
+            settings = angular.extend({
+                threshold: 64,
+                position: 0,
+                totalCount: 0
+            }, settings);
+
+            return {
+                settings: settings,
+                context: {
+                    cursor: 0,
+                    page: 0,
+                    data: []
+                };
+
+            };
         };
     })
     .filter('vscrollView', [function () {
-        return function (data, context) {
+        var empty = [];
 
+        return function (data, scope) {
+            if (!data || !scope) {
+                return empty;
+            }
+
+            var settings = scope.settings;
+            var context = scope.context;
+
+            if (settings.totalCount) {
+                if (context.cursor <= settings.totalCount &&
+                        context.cursor !== settings.position) {
+
+                    settings.position = context.cursor;
+                    var view = context.data;
+
+                    var first = Math.max(settings.position + Math.min(settings.totalCount - (settings.position + settings.threshold), 0), 0);
+                    var last = Math.min(settings.position + settings.threshold, settings.totalCount);
+                    view.length = first - last;
+                    for (var i = first, j = 0; i < last; i++, j++) {
+                        view[j] = data[i];
+                    }
+                }
+
+                return view;
+            }
+
+            return empty;
         };
     }])
     .directive('vscroll', [function () {
