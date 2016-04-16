7c10dd850d2542940c5fcd5fae664b0987c3bf06
klumba12 <dmitry.sitnov@gmail.com>
2016-04-16 17:50:00 -0400

    Added row and column adding interfaces;

--- vscroll.js
+++ vscroll.js
@@ -23,149 +23,205 @@
    };
 
    angular.module('vscroll', [])
-      .service('vscroll', function () {
-         return function (settings) {
-            settings = angular.extend({
-               threshold: 64,
-               position: 0,
-               totalCount: 0,
-               update: angular.noop
-            }, settings);
+       .service('vscroll', function () {
+          return function (settings) {
+             settings = angular.extend({
+                threshold: 64,
+                position: 0,
+                totalCount: 0,
+                update: angular.noop
+             }, settings);
 
-            var container = {
-               cursor: 0,
-               page: 0,
-               items: [],
-               force: true,
+             var container = {
+                cursor: 0,
+                page: 0,
+                items: [],
+                force: true,
 
-               update: function (force) {
-                  var self = this,
-                     threshold = settings.threshold,
-                     cursor = container.cursor,
-                     prevPage = self.page,
-                     page = Math.round((cursor + threshold) / threshold) - 1;
+                update: function (force) {
+                   var self = this,
+                       threshold = settings.threshold,
+                       cursor = container.cursor,
+                       prevPage = self.page,
+                       page = Math.round((cursor + threshold) / threshold) - 1;
 
-                  if (force || page > prevPage) {
-                     self.page = page;
+                   if (force || page > prevPage) {
+                      self.page = page;
 
-                     settings.update(
-                        function (count) {
-                           settings.totalCount = count;
-                           self.force = true;
-                        },
-                        page,
-                        (Math.max(1, page) - prevPage) * threshold);
-                  }
-               },
+                      settings.update(
+                          function (count) {
+                             settings.totalCount = count;
+                             self.force = true;
+                          },
+                          page,
+                          (Math.max(1, page) - prevPage) * threshold);
+                   }
+                },
 
-               reset: function () {
-                  this.cursor = 0;
-                  this.page = 0;
-                  this.items = [];
-                  this.force = true;
-                  this.update(true);
-               }
-            };
+                reset: function () {
+                   this.cursor = 0;
+                   this.page = 0;
+                   this.items = [];
+                   this.force = true;
+                   this.update(true);
+                }
+             };
 
-            container.update(true);
+             container.update(true);
 
-            return {
-               settings: settings,
-               container: container
-            };
-         };
-      })
-      .filter('vscroll', [function () {
-         var empty = [];
+             return {
+                settings: settings,
+                container: container
+             };
+          };
+       })
+       .filter('vscroll', [function () {
+          var empty = [];
 
-         return function (data, context) {
-            if (!data || !context) {
-               return empty;
-            }
+          return function (data, context) {
+             if (!data || !context) {
+                return empty;
+             }
 
-            var settings = context.settings,
-               container = context.container,
-               position = settings.position,
-               cursor = container.cursor,
-               threshold = settings.threshold,
-               totalCount = settings.totalCount;
+             var settings = context.settings,
+                 container = context.container,
+                 position = settings.position,
+                 cursor = container.cursor,
+                 threshold = settings.threshold,
+                 totalCount = settings.totalCount;
 
-            container.update();
+             container.update();
 
-            if (totalCount) {
-               if (container.force || (cursor <= totalCount && cursor !== position)) {
+             if (totalCount) {
+                if (container.force || (cursor <= totalCount && cursor !== position)) {
 
-                  settings.position = cursor;
+                   settings.position = cursor;
 
-                  var view = container.items,
-                     first = Math.max(cursor + Math.min(totalCount - (cursor + threshold), 0), 0),
-                     last = Math.min(cursor + threshold, totalCount);
+                   var view = container.items,
+                       first = Math.max(cursor + Math.min(totalCount - (cursor + threshold), 0), 0),
+                       last = Math.min(cursor + threshold, totalCount);
 
-                  view.length = last - first;
-                  for (var i = first, j = 0; i < last; i++, j++) {
-                     view[j] = data[i];
-                  }
+                   view.length = last - first;
+                   for (var i = first, j = 0; i < last; i++, j++) {
+                      view[j] = data[i];
+                   }
 
-                  container.force = false;
-               }
+                   container.force = false;
+                }
 
-               return view;
-            }
+                return view;
+             }
 
-            return empty;
-         };
-      }])
-      .directive('vscroll', [function () {
-         return {
-            restrict: 'A',
-            controller: [function () {
-            }],
-            link: function (scope, element, attrs) {
+             return empty;
+          };
+       }])
+       .directive('vscroll', [function () {
+          return {
+             restrict: 'A',
+             controller: [function () {
+             }],
+             link: function (scope, element, attrs) {
 
-            }
-         };
-      }])
-      .directive('vscrollPort', [function () {
-         return {
-            restrict: 'A',
-            controller: ['$scope', function ($scope) {
-               this.markup = {
-                  begin: null,
-                  end: null
-               };
-            }],
-            require: ['^vscroll'],
-            link: function (scope, element, attrs, ctrls) {
-               var port = ctrls[0];
+             }
+          };
+       }])
+       .directive('vscrollPort', [function () {
+          return {
+             restrict: 'A',
+             controller: ['$scope', function ($scope) {
+                this.markup = {
+                   begin: null,
+                   end: null
+                };
 
-               scope.on('$destroy', function () {
-                  delete port.markup;
-               });
-            }
-         };
-      }])
-      .directive('vscrollItem', [function () {
-         return {
-            restrict: 'A',
-            require: '^vscrollPort',
-            link: function (scope, element, attrs) {
+                this.setRow = function (index, element) {
 
-            }
-         };
-      }])
-      .directive('vscrollMark', [function () {
-         return {
-            restrict: 'A',
-            require: '^vscrollPor',
-            link: function (scope, element, attrs, ctrls) {
-               var port = ctrls[0];
-               port.markup[attr.vscrollMark] = element;
-               scope.on('$destroy', function () {
-                  if (port.markup) {
-                     port.markup[attr.vscrollMark] = null;
-                  }
-               });
-            }
-         };
-      }]);
+                };
+
+                this.removeRow = function (index, element) {
+
+                };
+
+                this.setColumn = function (index, element) {
+
+                };
+
+                this.removeColumn = function (index, element) {
+
+                };
+             }],
+             require: ['^vscroll'],
+             link: function (scope, element, attrs, ctrls) {
+                var port = ctrls[0];
+
+                scope.on('$destroy', function () {
+                   delete port.markup;
+                });
+             }
+          };
+       }])
+       .directive('vscrollRow', [function () {
+          return {
+             restrict: 'A',
+             require: '^vscrollPort',
+             link: function (scope, element, attrs, ctrls) {
+                var port = ctrls[0],
+                    index = parseInt(attrs.vscrollRow),
+                    item = {
+                       height: function () {
+                          return element.outerHeight(true);
+                       },
+                       width: function () {
+                          return element.outerWidth(true);
+                       }
+                    };
+
+                port.setRow(index, item);
+
+                scope.on('$destroy', function () {
+                   port.removeRow(index);
+                });
+             }
+          };
+       }])
+       .directive('vscrollColumn', [function () {
+          return {
+             restrict: 'A',
+             require: '^vscrollPort',
+             link: function (scope, element, attrs, ctrls) {
+                var port = ctrls[0],
+                    index = parseInt(attrs.vscrollColumn),
+                    item = {
+                       height: function () {
+                          return element.outerHeight(true);
+                       },
+                       width: function () {
+                          return element.outerWidth(true);
+                       }
+                    };
+
+                port.setColumn(index, item);
+
+                scope.on('$destroy', function () {
+                   port.removeColumn(index);
+                });
+             }
+          };
+       }])
+       .directive('vscrollMark', [function () {
+          return {
+             restrict: 'A',
+             require: '^vscrollPort',
+             link: function (scope, element, attrs, ctrls) {
+                var port = ctrls[0];
+                port.markup[attr.vscrollMark] = element;
+
+                scope.on('$destroy', function () {
+                   if (port.markup) {
+                      port.markup[attr.vscrollMark] = null;
+                   }
+                });
+             }
+          };
+       }]);
 })(angular);
