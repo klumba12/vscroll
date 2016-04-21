77ebc86d9ba1ce1a58a907807117486879a91c73
Valentin Korolev <kor.val@inbox.ru>
2016-04-21 15:22:24 +0300

    Bower dependency added.

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,11 @@ const jasmineBrowser = require('gulp-jasmine-browser');
 gulp.task('test', function () {
     gulp.src(['bower_components/angular/angular.js', 'bower_components/angular-mocks/angular-mocks.js', 'vscroll.js', 'test/*.js'])
         .pipe(jasmineBrowser.specRunner({ console: true }))
-        .pipe(jasmineBrowser.headless()); 
+        .pipe(jasmineBrowser.headless());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['vscroll.js', 'test/*.js'], function() {
+        gulp.run('test');
+    });  
 });
